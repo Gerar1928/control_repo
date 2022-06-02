f5eb718c7a2e9e82befb3ec2331f952dc1627a17
Gerar Almonte <Gerar1928@gmail.com>
2022-06-01 20:50:28 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,4 @@
 node default {
-  file { '/root/test.txt':
-    ensure  => file,
-    content => 'hello world',
-    owner   => 'root',
-  }
 }
 node 'master.puppet.vm' {
   include role::master_server
